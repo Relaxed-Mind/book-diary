a06acfa8293e880ab1a726878014c7cfc8840b69
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-10-06 16:34:46 +0900

    init && S3 setting

---
+++ src/main/java/capstone/bookdiary/BookdiaryApplication.java
@@ -0,0 +1,13 @@
+package capstone.bookdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookdiaryApplication.class, args);
+	}
+
+}
