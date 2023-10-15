da7b61f43696fd180cacf50f0ca62c691dc0ddc6
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-10-15 16:40:33 +0900

    swagger(spring doc) && querydsl init

---
+++ src/main/java/capstone/bookdiary/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package capstone.bookdiary.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Book-Diary Api Specification")
+                .description("2023-2 컴퓨터과학종합설계 책일기 API명세서")
+                .version("1.0.0");
+    }
+}
