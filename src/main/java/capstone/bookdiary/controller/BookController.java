41e125c45236137b4d9ca93751cd68e85145c6bd
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-11-05 09:48:58 +0900

    feat: add swagger at book search api 책 검색 api에 해당하는 swagger 작성 book search api #3

--- src/main/java/capstone/bookdiary/controller/BookController.java
+++ src/main/java/capstone/bookdiary/controller/BookController.java
@@ -1,6 +1,8 @@
 package capstone.bookdiary.controller;
 
 import capstone.bookdiary.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
@@ -13,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "책 컨트롤러", description = "책 관련 API")
 
 public class BookController {
     private final BookService bookService;
 
 
     @GetMapping("/search/{title}")
+    @Operation(summary = "책 제목 검색", description = "사용자가 책 제목을 넣어 검색합니다.")
     public ResponseEntity<Map<String, Object>> searchBook(@PathVariable String title){
         return ResponseEntity
                 .ok()
