907721b188f83d199db1e3732a226d259597b56d
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-11-14 16:26:35 +0900

    docs: add description in user api 유저 관련 스웨거 추가

--- src/main/java/capstone/bookdiary/controller/MemberController.java
+++ src/main/java/capstone/bookdiary/controller/MemberController.java
@@ -3,6 +3,7 @@ package capstone.bookdiary.controller;
 import capstone.bookdiary.domain.dto.LoginDto;
 import capstone.bookdiary.domain.dto.SignupDto;
 import capstone.bookdiary.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
+    @Operation(summary = "회원가입", description = "사용자가 회원가입을 합니다.")
     public ResponseEntity<Map<String, Object>> signup(@RequestBody SignupDto signupDto){
         return ResponseEntity
                 .ok()
@@ -28,6 +30,7 @@ public class MemberController {
     }
 
     @PostMapping("/signin")
+    @Operation(summary = "로그인", description = "사용자가 이메일과 패스워드를 넣어 로그인을 시도합니다.")
     public ResponseEntity<Map<String, Object>> signin(@RequestBody LoginDto loginDto) {
         return ResponseEntity
                 .ok()
