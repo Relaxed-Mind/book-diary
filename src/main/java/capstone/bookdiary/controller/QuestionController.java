deec3c7309d6786cf6ed66e178fdbbaba38a98c8
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-12-08 11:13:02 +0900

    feat: generate questions - 1차 질문 생성 - 2차 질문 생성

---
+++ src/main/java/capstone/bookdiary/controller/QuestionController.java
@@ -0,0 +1,38 @@
+package capstone.bookdiary.controller;
+
+import capstone.bookdiary.service.QuestionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Tag(name = "질문 컨트롤러", description = "질문 생성 관련 API")
+
+public class QuestionController {
+    private final QuestionService questionService;
+
+    @GetMapping("/questions/first/{bookDiaryId}")
+    @Operation(summary = "[미완성] 1차 질문 생성", description = "스크랩을 바탕으로 1차 질문을 생성합니다.")
+    private ResponseEntity<Map<String, Object>> generateFirstQuestions(@PathVariable Long bookDiaryId){
+        return ResponseEntity
+                .ok()
+                .body(questionService.generateFirstQuestions(bookDiaryId));
+    }
+
+    @GetMapping("/questions/second/{bookDiaryId}")
+    @Operation(summary = "[미완성] 2차 질문 생성", description = "스크랩과 1차 답변을 바탕으로 2차 질문을 생성합니다.")
+    private ResponseEntity<Map<String, Object>> generateSecondQuestions(@PathVariable Long bookDiaryId){
+        return ResponseEntity
+                .ok()
+                .body(questionService.generateSecondQuestions(bookDiaryId));
+    }
+
+}
---
+++ src/main/java/capstone/bookdiary/service/QuestionService.java
@@ -0,0 +1,74 @@
+package capstone.bookdiary.service;
+
+import capstone.bookdiary.domain.dto.ScrapResponseDto;
+import capstone.bookdiary.domain.entity.BookDiary;
+import capstone.bookdiary.domain.entity.Question;
+import capstone.bookdiary.exception.exceptions.DataNotFoundException;
+import capstone.bookdiary.repository.BookDiaryRepository;
+import capstone.bookdiary.repository.QuestionRepository;
+import capstone.bookdiary.repository.ScrapRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+    private final ScrapRepository scrapRepository;
+    private final QuestionRepository questionRepository;
+    private final BookDiaryRepository bookDiaryRepository;
+
+    public Map<String, Object> generateFirstQuestions(Long bookDiaryId){
+        //키워드 받아오기
+//        Map<String, Object> json = keywordClient.getKeyword(new KeywordParam(libraryKey,"json"));
+//        System.out.println("json.get(\"response\") = " + json.get("response"));
+
+        //스크랩 받아오기
+        List<ScrapResponseDto> scrapList = scrapRepository.findByBookDiary(bookDiaryId);
+        StringBuilder scrapMemo = new StringBuilder();
+        for (ScrapResponseDto scrapResponseDto : scrapList) {
+            scrapMemo.append("\"").append(scrapResponseDto.getContent()).append("\"").append("[").append(scrapResponseDto.getMemo()).append("] ");
+        }
+        System.out.println(scrapMemo);
+
+        //TODO: AI로 전송
+        Map<String, Object> data = new HashMap<>();
+        data.put("scrapMemo", scrapMemo);
+
+        //TODO: 응답 받아서 질문들 리턴
+        return data;
+    }
+
+    public Map<String, Object> generateSecondQuestions(Long bookDiaryId) {
+        //스크랩 받아오기
+        List<ScrapResponseDto> scrapList = scrapRepository.findByBookDiary(bookDiaryId);
+        StringBuilder scrapMemo = new StringBuilder();
+        for (ScrapResponseDto scrapResponseDto : scrapList) {
+            scrapMemo.append("\"").append(scrapResponseDto.getContent()).append("\"").append("[").append(scrapResponseDto.getMemo()).append("] ");
+        }
+
+        //1차 질문 받아오기
+        BookDiary bookDiary = bookDiaryRepository.findById(bookDiaryId)
+                .orElseThrow(DataNotFoundException::new);
+        List<Question> questions = questionRepository.findAllByBookDiary(bookDiary);
+        StringBuilder question = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+        for(int i=0; i<questions.size(); i++){
+            Question questionAnswer = questions.get(i);
+            question.append(i+1).append(".").append(questionAnswer.getQuestion()).append("\n");
+            answer.append("답변").append(i + 1).append(": ").append(questionAnswer.getAnswer()).append("\n");
+        }
+        System.out.println("question = " + question.toString());
+        System.out.println("answer = " + answer.toString());
+
+        Map<String, Object> request = new HashMap<>();
+        request.put("scrapMemo", scrapMemo.toString());
+        request.put("question", question.toString());
+        request.put("answer", answer.toString());
+
+        //2차 질문 return
+        return request;
+    }
+}
