24437a14378ab3e60ebf6feaf0323b69c6c04efb
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-12-11 23:44:03 +0900

    fix: delete cascase in scrap to image

--- src/main/java/capstone/bookdiary/service/ScrapService.java
+++ src/main/java/capstone/bookdiary/service/ScrapService.java
@@ -2,12 +2,15 @@ package capstone.bookdiary.service;
 
 import capstone.bookdiary.domain.dto.ScrapRequestDto;
 import capstone.bookdiary.domain.entity.BookDiary;
+import capstone.bookdiary.domain.entity.Image;
 import capstone.bookdiary.domain.entity.Scrap;
 import capstone.bookdiary.exception.exceptions.DataNotFoundException;
 import capstone.bookdiary.repository.BookDiaryRepository;
+import capstone.bookdiary.repository.ImageRepository;
 import capstone.bookdiary.repository.ScrapRepository;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScrapService {
     private final BookDiaryRepository bookDiaryRepository;
     private final ScrapRepository scrapRepository;
+    private final ImageRepository imageRepository;
+
     public Map<String, Object> addScrap(Long bookDiaryId, ScrapRequestDto scrapRequestDto) {
         BookDiary bookDiary = bookDiaryRepository.findById(bookDiaryId)
                 .orElseThrow(DataNotFoundException::new);
@@ -42,6 +47,12 @@ public class ScrapService {
 
     @Transactional
     public Map<String, Object> deleteScrap(Long scrapId) {
+        Scrap scrap = scrapRepository.findById(scrapId)
+                .orElseThrow(DataNotFoundException::new);
+        Optional<Image> optionalImage = imageRepository.findByScrap(scrap);
+        if(optionalImage.isPresent()){
+            imageRepository.delete(optionalImage.get());
+        }
         scrapRepository.deleteById(scrapId);
 
         Map<String, Object> response = new HashMap<>();
