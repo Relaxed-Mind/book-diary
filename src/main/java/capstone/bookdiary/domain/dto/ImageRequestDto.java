7f86f4a0bd09a7f864d0bc4d83d433c45db02214
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-12-11 13:39:45 +0900

    feat: expand scrap to 256

--- src/main/java/capstone/bookdiary/domain/dto/ImageRequestDto.java
+++ src/main/java/capstone/bookdiary/domain/dto/ImageRequestDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class ImageRequestDto {
     private Long scrapId;
-    @Size(max = 200, message = "스크랩 내용은 200자 이내로 입력해주세요.")
+    @Size(max = 256, message = "스크랩 내용은 256자 이내로 입력해주세요.")
     private String content;
     @Size(max = 500, message = "메모는 500자 이내로 입력해주세요.")
     private String memo;
